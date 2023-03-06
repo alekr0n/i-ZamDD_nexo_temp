653758338670ee7d60cff02be4dc606de0f8edee
Michał Romańczyk <miroman@i-sys.pl>
2023-03-06 13:47:49 +0100

    next

---
+++ main_ctrl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Telerik.Windows.Controls;
+
+namespace TelerikWpfApp3
+{
+    public partial class main_ctrl : UserControl
+    {
+        public main_ctrl()
+        {
+            InitializeComponent();
+        }
+
+        private void RadButtonClik(object sender, System.Windows.RoutedEventArgs e)
+        {
+            test test = new test();
+            test.Show();
+        }
+    }
+}
