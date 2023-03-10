f560f442f6b7ecf7536ecb42f3e8e32d0199bb42
Michał Romańczyk <miroman@i-sys.pl>
2023-03-10 12:10:03 +0100

    +GridView +Napisy w multiColumnBox -comboBox +RadComboBox

---
+++ Data.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelerikWpfApp3
+{
+    public class Data
+    {
+        ObservableCollection<Grupa> grupy = new ObservableCollection<Grupa>();
+        #region Grupa manipulation
+        public void EntryGrupaData()
+        {
+            grupy.Add(new Grupa()
+            {
+                Nazwa = "pomadki"
+            });
+            grupy.Add(new Grupa()
+            {
+                Nazwa = "Woda"
+            });
+            grupy.Add(new Grupa()
+            {
+                Nazwa = "dezedoranty"
+            });
+        }
+        public ObservableCollection<Grupa> GetGrupa()
+        {
+            grupy.Clear();
+            EntryGrupaData();
+            return this.grupy;
+        }
+        #endregion
+    }
+}
---
+++ Grupa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelerikWpfApp3
+{
+    public class Grupa
+    {
+        private string nazwa = String.Empty;
+
+        public string Nazwa
+        {
+            get
+            {
+                return this.nazwa;
+            }
+            set
+            {
+                if(this.nazwa != value)
+                {
+                    this.nazwa = value;
+                }
+            }
+        }
+    }
+}
